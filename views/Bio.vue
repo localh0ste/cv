084584a10a0682455ffbe6a6295bdfc2bf3cc4a5
localh0ste <vikrampawar2104@gmail.com>
2024-11-09 15:36:45 +0530

    cv done

---
+++ views/Bio.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+    <img class="w-full h-auto" src="@/assets/Biography/self.jpeg" />
+    <h2 class="font-bold text-2xl py-5">Vikram Pawar aka localh0ste</h2>
+    <h4 class="text-gray-600 font-medium text-xs" style="padding-bottom: 10px">
+        cyber security enthusiast 
+    </h4>
+    <h4 class="text-gray-600 font-medium text-xs">Delhi 📍</h4>
+    <div class="flex flex-wrap mt-5 gap-2 items-center">
+        <img class="w-auto h-5 rounded-none text-xs" alt="Swift" src="https://img.shields.io/badge/-Swift-orange" />
+        <img class="w-auto h-5 rounded-none text-xs" alt="Python" src="https://img.shields.io/badge/-Python-blue" />
+        <img class="w-auto h-5 rounded-none text-xs" alt="JavaScript" src="https://img.shields.io/badge/-Javascript-yellow" />
+        <img class="w-auto h-5 rounded-none text-xs" alt="SQL" src="https://img.shields.io/badge/-SQL-lightgrey" />
+        <img class="w-auto h-5 rounded-none text-xs" alt="Git" src="https://img.shields.io/badge/-Git-critical" />
+        <img class="w-auto h-5 rounded-none text-xs" alt="HTML/CSS" src="https://img.shields.io/badge/-HTML%2FCSS-yellowgreen" />
+        <img class="w-auto h-5 rounded-none text-xs" alt="Flask" src="https://img.shields.io/badge/-Flask-406893" />
+        <img class="w-auto h-5 rounded-none text-xs" alt="MongoDB" src="https://img.shields.io/badge/-MongoDB-4ca158" />
+    </div>
+    <div class="pt-7">
+        <h3 class="underline font-bold text-md pb-1">About Me</h3>
+        <p class="font-thin text-sm pb-2.5">
+            I’m a {{ new Date().getFullYear() - 2004 }} year old  a passionate Cybersecurity enthusiast and current BTech student specializing in Cybersecurity . 
+        </p>
+      
+    </div>
+    <div class="pt-7">
+        <h3 class="underline font-bold text-md pb-1">About Me</h3>
+    </div>
+    <div class="pt-7">
+        
+        <p class="font-thin text-sm pb-2.5">
+            I am focused on gaining exposure and advancing in the field of Governance, Risk, and Compliance (GRC). With a strong commitment to developing my skills, I am preparing for the OSCP and CISA certifications. Eager to learn from industry professionals and collaborate on challenging projects, I am driven to contribute meaningfully to the cybersecurity landscape.
+        </p>
+    </div>
+
+   
+
+
+    <div class="pt-7">
+        <h3 class="pb-1"><b>Résumé</b></h3>
+        <a style="color: #ff5733" href="/files/vikram_resume.pdf" class="font-thin text-sm pb-2.5">Check out my résumé here.</a>
+    </div>
+</div>
+</template>
